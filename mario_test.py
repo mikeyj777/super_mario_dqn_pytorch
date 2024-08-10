d8c7009189986aa9a6bda539f9ac9d8aedec19e4
Michael James <mikeyj777@gmail.com>
2024-08-10 11:34:01 -0400

    training rev 2

--- mario_test.py
+++ mario_test.py
@@ -2,29 +2,46 @@ import itertools
 import gym_super_mario_bros
 from agent import Mario
 
+from visual_preprocessing import *
+
+# NES Emulator for OpenAI Gym
+from nes_py.wrappers import JoypadSpace
+
 env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", render_mode='human', apply_api_compatibility=True)
+
+# Limit the action-space to
+#   0. walk right
+#   1. jump right
+env = JoypadSpace(env, [["right"], ["right", "A"]])
 
 mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=None)
 mario.load_state_dict(path_state_dict='trained_model/mario_net_580.chkpt')
 mario.net.eval()
 mario.exploration_rate = -1
 
+# Apply Wrappers to environment
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env)
+env = ResizeObservation(env, shape=84)
 
-state = env.reset()[0]
+env = FrameStack(env, num_stack=4)
 
-# Play the game!
-while True:
-    env.render()
-    # Run agent on the state
-    action = mario.act(state)
+for e in itertools.count():
+    state = env.reset()[0]
 
-    # Agent performs action
-    state, reward, done, trunc, info = env.step(action)
+    # Play the game!
+    while True:
+        env.render()
+        # Run agent on the state
+        action = mario.act(state)
 
-    # Check if end of game
-    if done or info["flag_get"]:
-        break
+        # Agent performs action
+        state, reward, done, trunc, info = env.step(action)
+
+        # Check if end of game
+        if done or info["flag_get"]:
+            break
+
+        apple = 1
 
     apple = 1
-
-apple = 1
